e98c97b6d6d494cba9e30d2dd2fc0a89a8fff52a
RBLONESTAR <ronald.beck@lonestar.edu>
2026-09-04 10:53:00 -0500

    Initialize FlowerGarden class structure
    Created FlowerGarden class with methods for managing flowers.

---
+++ FlowerGarden.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class FlowerGarden {
+
+    private ArrayList<String> garden;
+
+    public FlowerGarden() {
+        // TODO: Initialize garden
+    }
+
+    public void addFlower(String name) {
+        // TODO: Implement this method
+    }
+
+    public int findFlower(String name) {
+        // TODO: Implement this method
+        return -1;
+    }
+
+    public int getCount(String name) {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    public String getFlowerName(int index) {
+        // TODO: Implement this method
+        return "";
+    }
+
+    public String maxFlower() {
+        // TODO: Implement this method
+        return "";
+    }
+
+    public boolean inRange(int value, int low, int high) {
+        // TODO: Implement this method
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: Implement this method
+        return "";
+    }
+}
